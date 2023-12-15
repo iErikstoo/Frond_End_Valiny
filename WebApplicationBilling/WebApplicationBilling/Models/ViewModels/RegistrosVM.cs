96bce2d0fb3359b5d1e6eff4409cf8858cba4dfa
Daniel769476904 <jeam.nieto769@gmail.com>
2023-12-15 10:49:55 -0500

    conf(front-end): add registrosvm

---
+++ WebApplicationBilling/WebApplicationBilling/Models/ViewModels/RegistrosVM.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using WebApplicationBilling.Models.DTO;
+
+namespace WebApplicationBilling.Models.ViewModels
+{
+    public class RegistrosVM
+    {
+        public IEnumerable<SelectListItem> ListRegistros { get; set; }
+        public RegistroDTO Registro { get; set; }
+
+    }
+}
